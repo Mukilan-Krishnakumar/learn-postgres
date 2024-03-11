95da6fcfd53d062711d680c926909e3649d37694
Mukilan Krishnakumar <mukilan.k@supporthive.com>
2024-03-11 14:10:20 +0530

    Added Values to Customer Table

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Learn PostgreSQL
 
 1. [Create Customer Table](./scripts/1-create-customer-table.sql)
+2. [Add Data to Customer Table](./scripts/2-insert-customers.sql)
---
+++ scripts/2-insert-customers.sql
@@ -0,0 +1,2 @@
+INSERT INTO customer(first_name, last_name, email, company, street, city, state, zip, phone, birth_date, sex, date_entered)
+VALUES ('Monkey D.', 'Luffy', 'sungodnika@freedom.com', 'Straw Hats', '12, Beach View', 'East Blue', 'Dawn Island', '30444','2312223124', '1990-05-05', 'M', current_timestamp);
